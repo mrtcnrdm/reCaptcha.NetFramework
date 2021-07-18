3bad6bb35d0264d6bd703c9399a7a4d96baf0b2b
mrtcnrdm <mrtcnrdmii@gmail.com>
2021-07-18 10:23:23 +0300

    reCaptcha added

--- reCaptcha.NetFramework/Controllers/HomeController.cs
+++ reCaptcha.NetFramework/Controllers/HomeController.cs
@@ -1,30 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Newtonsoft.Json;
+using System.Net;
 using System.Web.Mvc;
 
 namespace reCaptcha.NetFramework.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(Models.reCaptcha response)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            Recaptcha();
+            if (check)
+            {
+                //ok
+                return View();
+            }
+            else
+            {
+                //error
+                TempData["Message"] = "Lütfen güvenliği doğrulayınız.";
+                return View();
+            }
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        private bool check;
 
-            return View();
+        public void Recaptcha()
+        {
+            var response = Request["g-recaptcha-response"];
+            string secretKey = "secret-key";
+            var client = new WebClient();
+            var GoogleReply = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey, response));
+
+            var captchaResponse = JsonConvert.DeserializeObject<Models.reCaptcha>(GoogleReply);
+            if (captchaResponse.Success)
+
+                check = true;
+            else
+
+                check = false;
         }
     }
 }
